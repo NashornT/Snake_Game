bf6bc4985a91eadf8b6a040d7145661eccfc0556
Gustavo Barboza <Huedor123@gmail.com>
2022-05-31 21:02:01 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -52,7 +52,7 @@ function Start(){
     for(i = 1; i < snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
             clearInterval(jogo);
-            alert('Game Over!');
+            alert('Ainda perdeu, VERGONHA !!');
         }
     }
 
